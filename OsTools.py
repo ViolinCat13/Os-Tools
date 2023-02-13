7e43dfcf0900e99fd3e0d82da5351762244c10e8
TheBlockSmith12 <125318356+ViolinCat13@users.noreply.github.com>
2023-02-13 22:53:43 +0000

    Added try, except statements to handle errors.

--- OsTools.py
+++ OsTools.py
@@ -1,78 +1,90 @@
 import os
 
+
 class FileHandler:
-  
-  def __init__(self, permission):
-    self.permission = permission
 
-  def backup(self, orig, back):
-    with open(orig) as f:
-      with open(back, "w") as f1:
-        for line in f:
-          f1.write(line)
-  
-  def permission_error(self):
-    raise Exception('This File Handler Does Not Have Permission To Execute This Function!')
+    def __init__(self, permission):
+        self.permission = permission
 
-  def wipe(self, file, back=False):
-    if self.permission != 'Admin':
-      self.permission_error()
-    elif self.permission == "Admin":
-      if back:
-        self.backup(file, f"backup-of-{file}.txt")
-      file = open(file, "w+")
-      file.write('')
-      file.close()
+    def backup(self, orig, back):
+        with open(orig) as f:
+            with open(back, "w") as f1:
+                for line in f:
+                    f1.write(line)
 
-  def wipeFiles(self, files, back=False):
-    if self.permission != 'Admin':
-      self.permission_error()
-    elif self.permission == "Admin":
-      if back:
-        self.backup(file, f"backup-of-{file}.txt")
-      for file in files:
-        file = open(file, "w+")
-        file.write('')
-        file.close()
+    def permission_error(self):
+        raise Exception('This File Handler Does Not Have Permission To Execute This Function!')
 
-  def appendFile(self, file, dest):
-    with open(file, 'r+') as file:
-      with open(dest, 'a+') as dest:
-        toAppend = file.readlines()
-        dest.writelines(toAppend)
-        file.close()
-        dest.close()
-        
-  def createFile(self, name, content=''):
-    file = open(name, 'x')
-    if content != '':
-      file.writelines(content)
+    def wipe(self, file, back=False):
+        if self.permission != 'Admin':
+            self.permission_error()
+        elif self.permission == "Admin":
+            if back:
+                self.backup(file, f"backup-of-{file}.txt")
+            file = open(file, "w+")
+            file.write('')
+            file.close()
 
-  def deleteFile(self, file):
-    os.remove(file)
+    def wipeFiles(self, files, back=False):
+        if self.permission != 'Admin':
+            self.permission_error()
+        elif self.permission == "Admin":
+            for file in files:
+                if back:
+                    self.backup(file, f"backup-of-{file}.txt")
+                file = open(file, "w+")
+                file.write('')
+                file.close()
 
-  def deleteFiles(self, files):
-    for file in files:
-      os.remove(file)
+    def appendFile(self, file, dest):
+        with open(file, 'r+') as file:
+            with open(dest, 'a+') as dest:
+                toAppend = file.readlines()
+                dest.writelines(toAppend)
+                file.close()
+                dest.close()
+
+    def createFile(self, name, content=''):
+        try:
+            file = open(name, 'x')
+        except FileExistsError as error:
+            print('A file with this name already exists!\nError:', error)
+        if content != '':
+            file.writelines(content)
+
+    def deleteFile(self, file):
+        try:
+            os.remove(file)
+        except FileNotFoundError as error:
+            print('The file:', file, 'does not exist.\nError:', error)
+
+    def deleteFiles(self, files):
+        for file in files:
+            try:
+                os.remove(file)
+            except FileNotFoundError as error:
+                print('The file:', file, 'does not exist.\nError:', error)
+
 
 class VarHandler:
 
-  def __init__(self, permission):
-    permission = permission
+    def __init__(self, permission):
+        permission = permission
 
-  def clear(self, var):
-    var = None
-    return var
+    def clear(self, var):
+        var = None
+        return var
+
 
 class EasyLooper:
 
-  def repeat(self, func, loops):
-    for i in range(loops):
-      func()
+    def repeat(self, func, loops):
+        for i in range(loops):
+            func()
 
-  def repeatparam(self, func, param, loops):
-    for i in range(loops):
-      print(param)
-      param = func(param)
-    return param
+    def repeatParam(self, func, param, loops):
+        for i in range(loops):
+            print(param)
+            param = func(param)
+        return param
 
